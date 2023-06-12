1c6683b85929e52cc0277ed8e82415024f7ad282
Seth Schroeder <seth.schroeder@gmail.com>
2023-06-11 21:26:28 -0400

    Delete tracer and collapse common

--- lib/checks/runner.ex
+++ lib/checks/runner.ex
@@ -17,8 +17,6 @@ defmodule WarnMultiErrorHandlingInObanJob.Checks.Runner do
   The job will be considered a success.
   """
 
-  import WarnMultiErrorHandlingInObanJob.Common
-
   use Credo.Check,
     base_priority: :normal,
     category: :warning,
@@ -50,7 +48,7 @@ defmodule WarnMultiErrorHandlingInObanJob.Checks.Runner do
 
   # Simplify the code by looking only at relevant files
   defp traverse({:defmodule, _, body} = ast, issues, _, _) do
-    if continue_with_file?(body) do
+    if in_header?(body, :use, :Oban) do
       {ast, issues}
     else
       {{}, issues}
@@ -69,6 +67,59 @@ defmodule WarnMultiErrorHandlingInObanJob.Checks.Runner do
   defp traverse(ast, issues, _, _) do
     {ast, issues}
   end
+
+  defp walk({:|>, [line: _, column: _], args}, acc) do
+    Enum.reduce(args, acc, &walk/2)
+  end
+
+  defp walk({{:., _, [{:__aliases__, _, module}, method]}, _, body}, acc) do
+    acc =
+      case {module, method} do
+        {[:Ecto, :Multi], :new} -> Map.put(acc, module, method)
+        {[:Multi], :new} -> Map.put(acc, [:Ecto | module], method)
+        {[:Repo], :transaction} -> Map.put(acc, module, method)
+        _ -> acc
+      end
+
+    Enum.reduce(body, acc, &walk/2)
+  end
+
+  defp walk({:case, [line: _line, column: _column], [[do: clauses]]}, acc) do
+    Enum.reduce(clauses, acc, &look_at_error_handling/2)
+  end
+
+  # NOTE: special exception for commented out code being checked in -- I have deleted this waaaaaay too many times now.
+  #
+  # defp walk(zut_alors, acc) do
+  #   dbg(zut_alors)
+  #   acc
+  # end
+
+  defp walk(_, acc), do: acc
+
+  defp look_at_error_handling({:->, _, [[ok: _], _]}, acc), do: acc
+
+  defp look_at_error_handling({:->, _, [[{:{}, _, [:error, _, _, _]}], {:error, message}]}, acc) do
+    Map.put(acc, :error, message)
+  end
+
+  # This HAS to be improvable. It used to have more than one caller.
+  defp in_header?([{:__aliases__, _, _}, [do: {:__block__, [], header}]], section, module) do
+    header
+    |> Enum.filter(fn item ->
+      section == get_in(item, [Access.elem(0)])
+    end)
+    |> Enum.map(fn item ->
+      item
+      |> get_in([Access.elem(2)])
+      |> get_in([Access.at(0)])
+      |> get_in([Access.elem(2)])
+      |> get_in([Access.at(0)])
+    end)
+    |> Enum.any?(&Kernel.==(&1, module))
+  end
+
+  defp in_header?(_, _, _), do: false
 
   defp issues_for_function_definition([_, [do: body]], line, issues, _, issue_meta) do
     case walk(body, %{}) do
